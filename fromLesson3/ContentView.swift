3759a3c896aa1eb1f6a0f2813c05df4df6f4f22f
Tokarev Dmitry <modelist61@bk.ru>
2020-12-16 20:47:09 +0300

    Initial Commit

---
+++ fromLesson3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fromLesson3
+//
+//  Created by Dmitry Tokarev on 16.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fromLesson3/fromLesson3App.swift
@@ -0,0 +1,17 @@
+//
+//  fromLesson3App.swift
+//  fromLesson3
+//
+//  Created by Dmitry Tokarev on 16.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct fromLesson3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
